049a12a8f4f2942bcc2731392c5953e7534021aa
Muhammad Abbas <mabbasm1100@gmail.com>
2026-07-11 12:44:12 +0500

    Initial Commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <> 
+    {/* Known as fragments, used to contain content without creating extra divs */}
+    <div id='parent'>
+      <h1 id='child1'>I'm Abbas</h1>
+      <h2 id='child2'>I'm Ronaldo</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
